31fd4ec180a4956146046b9f71f8d8b3b3262f03
Peacebern <peace8282bern@gmail.com>
2024-06-03 23:35:01 +0100

    app.jsx fix to show same as landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import Navbar from '../src/components/Navbar.jsx';
+import Furniture from "./components/Furniture.jsx";
+import Products from "./components/Products.jsx";
+import BestServices from "./components/BestServices.jsx";
 
+import FeauturedPosts from './components/FeauturedPosts.jsx';
+import About from './components/About.jsx';
+import CallToAction from './components/CallToAction.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Furniture />
+      <Products />
+      <BestServices />
+      <FeauturedPosts />
+      <About />
+      <CallToAction />
+      <Footer />
     </div>
   )
 }
