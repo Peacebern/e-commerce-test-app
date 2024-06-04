260f1a5d30d97805ff116379bbff2632681e0837
Peacebern <peace8282bern@gmail.com>
2024-06-04 09:50:29 +0100

    prettier format

--- src/components/Furniture.jsx
+++ src/components/Furniture.jsx
@@ -6,36 +6,35 @@ import { Link } from "react-router-dom";
 
 export default function Furniture() {
     return(
-        <div>
-                <div className='fur-hero'>
-                    <div className="right-carousel">
-                        <p>5 Items</p>
-                        <h2>FURNITURE</h2>
-                        <Link to="#">Read More</Link>
-                    </div>
+        <div className='fur-hero'>
+            <div className="right-carousel">
+                <p>5 Items</p>
+                <h2>FURNITURE</h2>
+                <Link to="#">Read More</Link>
+            </div>
 
-                    <div className="left-carousel">
-                        <div className="top-carousel">
+            <div className="left-carousel">
+                <div className="top-carousel">
+                <p>5 Items</p>
+                <h3>FURNITURE</h3>
+                <Link to="#">Read More</Link>
+                </div>
+
+                <div className="bottom-carousel">
+                    <div className="bottom-carousel-one">
                         <p>5 Items</p>
                         <h3>FURNITURE</h3>
                         <Link to="#">Read More</Link>
-                        </div>
+                    </div>
 
-                        <div className="bottom-carousel">
-                        <div className="bottom-carousel-one">
-                            <p>5 Items</p>
-                            <h3>FURNITURE</h3>
-                            <Link to="#">Read More</Link>
-                        </div>
-
-                        <div className="bottom-carousel-two">
-                            <p>5 Items</p>
-                            <h3>FURNITURE</h3>
-                            <Link to="#">Read More</Link>
-                        </div>
+                    <div className="bottom-carousel-two">
+                        <p>5 Items</p>
+                        <h3>FURNITURE</h3>
+                        <Link to="#">Read More</Link>
                     </div>
                 </div>
             </div>
         </div>
+        
     );
 }
