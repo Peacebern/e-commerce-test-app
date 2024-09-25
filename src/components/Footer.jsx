b8c83dab33f57b95528a147205b6a8a18c27dfe1
Peacebern <peace8282bern@gmail.com>
2024-09-25 14:55:39 +0100

    finishing touchws on footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,62 +7,70 @@ import x from "../assets/Images/Footer-images/x.svg";
 
 function Footer() {
 return (
-    <div className='footer-max'>
-        <div className='footer-bg1'>
-            <div className='footer-hero'>
-                <div>
-                    <p>Bandage</p>
-                </div>
-                <div className='footer-img'>
-                    <img src={fb}/>
-                    <img src={ig} />
-                    <img src={x} />
-                </div>
-            </div>
-        </div>
-        <div className='footer-2'>
-            <div>
-                <h5>Company Info</h5>
-                <p>About Us</p>
-                <p>Carrier</p>
-                <p>We are hiring</p>
-                <p>Blog</p>
-            </div>
-            <div>
-                <h5>Legal</h5>
-                <p>About Us</p>
-                <p>Carrier</p>
-                <p>We are hiring</p>
-                <p>Blog</p>
-            </div>
-            <div>
-                <h5>Features</h5>
-                <p>Business Marketing</p>
-                <p>User Analytic</p>
-                <p>Live Chat</p>
-                <p>Unlimited Support</p>
-            </div>
-            <div>
-                <h5>Resources</h5>
-                <p>Ios & Android</p>
-                <p>Watch a Demo</p>
-                <p>Customers</p>
-                <p>API</p>
-            </div>
-            <div>
-                <h5>Get In Touch</h5>
-                <div action="/submit" method="post">
-                    <input type="email" id="email" name="email" value="Your Email" className='footer-input' required />
-                    <input type="submit" value="Subscribe" className='footer-input2' />
-                </div>
-                <p>Lore imp sum dolor Amit</p>
-
-            </div>
-        </div>
-        <div className='footer-foot'>
-            <p>Made With Love By Finland All Right Reserved </p>
-        </div>
-    </div>
+	<div className="footer-max">
+		<div>
+			<div className="footer-bg1">
+				<div className="footer-hero">
+					<div>
+						<p>Bandage</p>
+					</div>
+					<div className="footer-img">
+						<img src={fb} />
+						<img src={ig} />
+						<img src={x} />
+					</div>
+				</div>
+			</div>
+			<div className="footer-2">
+				<div>
+					<h5>Company Info</h5>
+					<p>About Us</p>
+					<p>Carrier</p>
+					<p>We are hiring</p>
+					<p>Blog</p>
+				</div>
+				<div>
+					<h5>Legal</h5>
+					<p>About Us</p>
+					<p>Carrier</p>
+					<p>We are hiring</p>
+					<p>Blog</p>
+				</div>
+				<div>
+					<h5>Features</h5>
+					<p>Business Marketing</p>
+					<p>User Analytic</p>
+					<p>Live Chat</p>
+					<p>Unlimited Support</p>
+				</div>
+				<div>
+					<h5>Resources</h5>
+					<p>Ios & Android</p>
+					<p>Watch a Demo</p>
+					<p>Customers</p>
+					<p>API</p>
+				</div>
+				<div>
+					<h5>Get In Touch</h5>
+					<div action="/submit" method="post">
+						<input
+							type="email"
+							id="email"
+							name="email"
+							value="Your Email"
+							className="footer-input"
+							required
+						/>
+						<input type="submit" value="Subscribe" className="footer-input2" />
+					</div>
+					<p>Lore imp sum dolor Amit</p>
+				</div>
+			</div>
+			<div className="footer-foot">
+				<p>Made With Love By Finland All Right Reserved </p>
+			</div>
+		</div>
+	</div>
 );
 }
 
