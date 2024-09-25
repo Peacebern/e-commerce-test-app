c0b8d1164735f48f5baa197f975687960aa062ff
Peacebern <peace8282bern@gmail.com>
2024-09-25 01:22:37 +0100

    finishing touches on hero section

--- src/components/Furniture.jsx
+++ src/components/Furniture.jsx
@@ -1,40 +1,53 @@
-import React from 'react';
+import React from "react";
 import "../styles/furniture.css";
 import { Link } from "react-router-dom";
 
-
-
 export default function Furniture() {
-    return(
-        <div className='fur-hero'>
-            <div className="right-carousel">
-                <p>5 Items</p>
-                <h2>FURNITURE</h2>
-                <Link to="#">Read More</Link>
-            </div>
+	return (
+		<div className="fur-hero">
+			<div className="right-carousel">
+				<div className="hero_text_cont">
+					<p>5 Items</p>
+					<h2>FURNITURE</h2>
+					<Link to="#">Read More</Link>
+				</div>
+				<div className="hero_image_container">
+					<img src="/Images/hero-fur-images/furcookware.svg" />
+				</div>
+			</div>
 
-            <div className="left-carousel">
-                <div className="top-carousel">
-                <p>5 Items</p>
-                <h3>FURNITURE</h3>
-                <Link to="#">Read More</Link>
-                </div>
+			<div className="top-carousel">
+				<div className="hero_image_container">
+					<img src="/Images/hero-fur-images/furbowel.svg" />
+				</div>
+				<div className="hero_text_cont">
+					<p>5 Items</p>
+					<h2>FURNITURE</h2>
+					<Link to="#">Read More</Link>
+				</div>
+			</div>
 
-                <div className="bottom-carousel">
-                    <div className="bottom-carousel-one">
-                        <p>5 Items</p>
-                        <h3>FURNITURE</h3>
-                        <Link to="#">Read More</Link>
-                    </div>
+			<div className="bottom-carousel-one">
+				<div className="hero_image_container">
+					<img src="/Images/hero-fur-images/furchandelier.svg" />
+				</div>
+				<div className="hero_text_cont">
+					<p>5 Items</p>
+					<h2>FURNITURE</h2>
+					<Link to="#">Read More</Link>
+				</div>
+			</div>
 
-                    <div className="bottom-carousel-two">
-                        <p>5 Items</p>
-                        <h3>FURNITURE</h3>
-                        <Link to="#">Read More</Link>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    );
+			<div className="bottom-carousel-two">
+				<div className="hero_image_container">
+					<img src="/Images/hero-fur-images/furvase.svg" />
+				</div>
+				<div className="hero_text_cont">
+					<p>5 Items</p>
+					<h2>FURNITURE</h2>
+					<Link to="#">Read More</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
